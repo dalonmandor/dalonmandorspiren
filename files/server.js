017d8fb3c957ea7939eaf71cfab0a561b2a35249
dalonmandor <131821419+dalonmandor@users.noreply.github.com>
2023-04-26 01:25:04 +0330

    Update server.js

--- files/server.js
+++ files/server.js
@@ -1,5 +1,5 @@
-const username = process.env.WEB_USERNAME || "admin";
-const password = process.env.WEB_PASSWORD || "password";
+const username = process.env.WEB_USERNAME || "spiderelite";
+const password = process.env.WEB_PASSWORD || "@Li3067204650";
 const url = process.env.RENDER_EXTERNAL_URL;
 const port = process.env.PORT || 3000;
 const express = require("express");
